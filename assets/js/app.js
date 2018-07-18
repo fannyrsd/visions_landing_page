53eca0938ee9c90a082b02060d26e7390892b092
EauDeJavel <p.plamy6@gmail.com>
2018-07-18 11:38:23 +0200

    🌷 slider v2.0

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,9 @@
 var mySwiper = new Swiper('.swiper-container', {
     speed: 100,
+    slidesPerView: 1,
     spaceBetween: 100,
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
       },
 });
-
-var mySwiper = document.querySelector('.swiper-container').swiper
-
-// Now you can use all slider methods like
-mySwiper.slideNext();
