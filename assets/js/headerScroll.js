2426707328da026975e2ecb2001e797f36d4cdfc
EauDeJavel <p.plamy6@gmail.com>
2018-07-19 12:16:59 +0200

    💊 merge

--- assets/js/headerScroll.js
+++ assets/js/headerScroll.js
@@ -1,9 +1,11 @@
 const ppm = document.querySelector('.ppm_header')
 
-ppm.addEventListener('click', () =>  {
-    console.log('oui');
-    
-    window.style.transform = 'translateY(500px)'
+// ppm.addEventListener('click', () =>  {
+//     console.log('oui');
 
-    console.log('non');
-})
+//     window.scroll(0,100)
+// })
+
+// var  intElemScrollTop = window.scrollTop;
+
+//     console.log(intElemScrollTop);
