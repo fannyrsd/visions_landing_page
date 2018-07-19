da98b14832191eff7541d4222b6f19e260698f58
EauDeJavel <p.plamy6@gmail.com>
2018-07-19 10:48:49 +0200

    🌷 fanny jtm

---
+++ assets/js/headerScroll.js
@@ -0,0 +1,9 @@
+const ppm = document.querySelector('.ppm_header')
+
+ppm.addEventListener('click', () =>  {
+    console.log('oui');
+    
+    window.style.transform = 'translateY(500px)'
+
+    console.log('non');
+})
