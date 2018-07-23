05b6b5f9dfcb4c4fcea10b10a35725d98cb62aaf
EauDeJavel <p.plamy6@gmail.com>
2018-07-23 15:41:42 +0200

    🌷 fix slider

--- assets/js/canvasIndex.js
+++ assets/js/canvasIndex.js
@@ -2,7 +2,7 @@
 particlesJS("particles-js", {
     "particles": {
       "number": {
-        "value": 1,
+        "value": 20,
         "density": {
           "enable": true,
           "value_area": 800
