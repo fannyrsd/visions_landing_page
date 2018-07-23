3ae239e59dd0fc6e34366b714fa905adba05b545
EauDeJavel <p.plamy6@gmail.com>
2018-07-23 10:57:30 +0200

    🌷 fix navigators bugs + fiw popups

--- assets/js/app.js
+++
@@ -1,9 +0,0 @@
-var mySwiper = new Swiper('.swiper-container', {
-    speed: 100,
-    slidesPerView: 1,
-    spaceBetween: 100,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-});
